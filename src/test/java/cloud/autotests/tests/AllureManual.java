617025c967c0d9342a69bb518cc0d99523616d99
Olga Zatulivetrova <zatulivetrova@gmail.com>
2022-02-01 12:03:46 +0300

    added manual for example

---
+++ src/test/java/cloud/autotests/tests/AllureManual.java
@@ -0,0 +1,26 @@
+/*
+///**package cloud.autotests.tests;
+
+
+import annotations.Manual;
+import io.qameta.allure.AllureId;
+import io.qameta.allure.Owner;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static io.qameta.allure.Allure.step;
+
+public class AllureManual {
+
+    @Test
+    @Manual("Ручные тесты")
+    @AllureId("6890")
+    @DisplayName("Страница переключается на английский язык")
+    @Owner("zatulivetrova")
+    public void switchToEnglish() {
+        step("Открыть страницу");
+        step("Переключить страницу на английский");
+        step("Проверить, что страница полностью переведена");
+    }
+}
+*/
