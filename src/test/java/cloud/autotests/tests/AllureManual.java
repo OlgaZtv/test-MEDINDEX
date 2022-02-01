c59318967cefadd41de9afc85be04253e34fac94
Olga Zatulivetrova <zatulivetrova@gmail.com>
2022-02-01 13:14:45 +0300

    added AllureManual

--- src/test/java/cloud/autotests/tests/AllureManual.java
+++ src/test/java/cloud/autotests/tests/AllureManual.java
@@ -1,11 +1,11 @@
-/*
-///**package cloud.autotests.tests;
+package cloud.autotests.tests;
 
 
 import annotations.Manual;
 import io.qameta.allure.AllureId;
 import io.qameta.allure.Owner;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 import static io.qameta.allure.Allure.step;
@@ -16,11 +16,20 @@ public class AllureManual {
     @Manual("Ручные тесты")
     @AllureId("6890")
     @DisplayName("Страница переключается на английский язык")
-    @Owner("zatulivetrova")
+    @Owner("lesnik0909")
     public void switchToEnglish() {
         step("Открыть страницу");
         step("Переключить страницу на английский");
         step("Проверить, что страница полностью переведена");
     }
+
+    @Test
+    @AllureId("6891")
+    @DisplayName("Вакансии для соискателей размещены на главной странице")
+    @Tag("main_page")
+    @Manual("Ручные тесты")
+    @Owner("lesnik0909")
+    public void vacanciesOnMainPage() {
+        step("На странице должна быть форма с вакансиями");
+    }
 }
-*/
